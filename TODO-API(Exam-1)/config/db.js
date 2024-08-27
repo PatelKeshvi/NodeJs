ddbfb48aae4ddfd97269983ccec08ba7874b0c11
Keshvi Patel <160455891+PatelKeshvi@users.noreply.github.com>
2024-08-27 17:53:17 +0530

    Delete TODO-API(Exam-1) directory

--- TODO-API(Exam-1)/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const dbConnect = async() => {
-    await mongoose.connect("mongodb+srv://ishitasurati83:todo12345@cluster0.enet2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    console.log("Connect to MongoDB");
-
-}
-
-module.exports = dbConnect
